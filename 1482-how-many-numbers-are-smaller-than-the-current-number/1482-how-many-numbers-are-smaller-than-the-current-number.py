1c544fd3ada78f3388e36fb065712631fa86ff15
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-08-02 13:08:09 -0900

    Time: 31 ms (92.04%), Space: 11.7 MB (21.11%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -5,12 +5,11 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        result = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[i] > nums[j]:
-                    count += 1
-            result.append(count)
+        sorted_nums = sorted(nums)
+        num_dict = {}
+        for idx, num in enumerate(sorted_nums):
+            if num not in num_dict:
+                num_dict[num] = idx
 
+        result = [num_dict[num] for num in nums]
         return result
