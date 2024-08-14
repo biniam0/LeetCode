5b55a4349014af70b36661252c7c6cfcb3d042e6
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-08-14 07:01:01 -0900

    Time: 29 ms (66.28%), Space: 11.9 MB (22.64%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,29 +1,22 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+from collections import defaultdict
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
         stack = []
-        next = defaultdict(lambda:-1)
-        
+        res = defaultdict(lambda: -1)
+
         for num in nums2:
             while stack and stack[-1] < num:
-                next[stack.pop()] = num
-            stack.append(num)
-        
-        return [next[num] for num in nums1]
-        
-        
-        
-        
-        # res = []
+                res[stack[-1]] = num
+                stack.pop()
 
-        # l = 0
-        # while l < len(nums1):
-        #     for r in range(len(nums2)):
-        #         if nums1[l] == nums2[r]:
-        #             if r+1 == len(nums2):
-        #                 res.append(-1)
-        #             elif nums2[r] < nums2[r+1]:
-        #                 res.append(nums2[r+1])
-        #             else:
-        #                 res.append(-1)
-        #     l += 1
-        # return res
+            stack.append(num)
+
+        return [res[num] for num in nums1]
+      
+
+        
