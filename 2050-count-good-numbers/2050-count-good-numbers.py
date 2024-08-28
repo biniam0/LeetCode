43d8e0bc72279f16369e1e82e1e9cae95c7f804c
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-08-28 10:44:02 +0300

    Time: 41 ms (19.34%), Space: 16.6 MB (13.08%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.py
+++ 2050-count-good-numbers/2050-count-good-numbers.py
@@ -1,11 +1,24 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
+        # Recursive Approach
+        # MOD = 10**9 + 7
+
+        # def countGood(n, i=0):
+        #     if n == 0:
+        #         return 1
+        #     if i % 2 == 0:
+        #         return 5 * countGood(n-1, i+1) % MOD
+        #     else:
+        #         return 4 * countGood(n-1, i+1) % MOD
+        # return countGood(n)
+
+        # Exponential Approach
         mod = 10**9 + 7
         even_pos = (n+1)//2
         prime_pos = n//2
 
         return (pow(5, even_pos, mod) * pow(4, prime_pos, mod))% mod
-        
+        # Iteration Approach
         # mod = 10**9 + 7
         # res = 1
         # for i in range(n):
