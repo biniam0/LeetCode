78a504c9d85b4de8374c677715f15b18d7e595a5
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-09-08 13:29:20 +0300

    Time: 35 ms (76.38%), Space: 16.5 MB (34.93%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,47 +1,24 @@
 # Definition for singly-linked list.
-class ListNode(object):
+class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
 
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        
-        # Determine the head of the merged list
-        if list1.val < list2.val:
-            head = list1
-            list1 = list1.next
-        else:
-            head = list2
-            list2 = list2.next
-        
-        # Current pointer to build the merged list
-        current = head
-        
-        # Traverse both lists and append the smaller value to the merged list
         while list1 and list2:
             if list1.val < list2.val:
-                current.next = list1
+                tail.next = list1
                 list1 = list1.next
             else:
-                current.next = list2
+                tail.next = list2
                 list2 = list2.next
-            current = current.next
-        
-        # If there are remaining nodes in either list, append them
-        if list1:
-            current.next = list1
-        elif list2:
-            current.next = list2
-        
-        # Return the head of the merged linked list
-        return head
+            tail = tail.next
+        if not list1:
+            tail.next = list2
+        elif not list2:
+            tail.next = list1
+
+        return dummy.next
