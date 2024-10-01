85b29a86fc01eb9701293c0e387f140be1e2a38b
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-10-01 12:02:44 +0300

    Time: 553 ms (7.14%), Space: 35.2 MB (5.3%) - LeetHub

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        freq = defaultdict(int)
+        for num in arr:
+            freq[((num % k)+k) % k] += 1
+        print(freq)
+        if freq[0] % 2 != 0:
+            return False
+
+        for i in range(1, k):
+            if freq[i] != freq[k-i]:
+                return False
+        return True
