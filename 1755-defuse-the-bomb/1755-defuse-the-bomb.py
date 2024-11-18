1874576f914aac6878c52526d285cbc048e0b2b5
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-11-18 06:17:37 -0900

    Time: 0 ms (100%), Space: 16.8 MB (22.92%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.py
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -3,12 +3,21 @@ class Solution:
         n = len(code)
         res = [0]*n
 
-        for i in range(n):
-            if k > 0:
-                for j in range(1, k+1):
-                    res[i] += code[(i+j)%n]
-            elif k < 0:
-                for j in range(1, abs(k)+1):
-                    res[i] += code[(i-j)%n]
-          
+        l = 0
+        cur_sum = 0
+        for r in range(n+abs(k)):
+            cur_sum += code[r%n]
+
+            if r -l+1 > abs(k):
+                cur_sum -= code[l%n]
+                l = (l+1) % n
+
+            if r - l +1 == abs(k):
+                if k >0:
+                    res[(l-1)%n] = cur_sum
+                elif k < 0:
+                    res[(r+1)%n] = cur_sum
+
+
+
         return res
