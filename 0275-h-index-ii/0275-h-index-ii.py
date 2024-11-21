0fa73dc8623ca0bf3e86f042d9f699698a298870
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-11-21 06:24:59 -0900

    Time: 0 ms (100%), Space: 22 MB (97.28%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,10 +1,23 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        h, n = 0, len(citations)
-    
-        for i in range(n-1, -1, -1):
-            if n-i <= citations[i]:
-                h += 1
+        # Binary Search Approach
+        n = len(citations)
+        l, r = 0, n-1
+        
+        while l <= r:
+            mid = (l+r)//2
+            if citations[mid] >= n - mid:
+                r = mid - 1
             else:
-                break
-        return h
+                l = mid + 1
+        return n - l
+
+        # Iterative Approach
+        # h, n = 0, len(citations)
+    
+        # for i in range(n-1, -1, -1):
+        #     if n-i <= citations[i]:
+        #         h += 1
+        #     else:
+        #         break
+        # return h
