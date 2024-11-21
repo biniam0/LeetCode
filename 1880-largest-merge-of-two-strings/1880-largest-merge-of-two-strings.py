a287d317f4074e3cbf5fa78c3f65883d9095cabf
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-11-21 10:13:28 -0900

    Time: 1075 ms (6.33%), Space: 16.9 MB (49.62%) - LeetHub

--- 1880-largest-merge-of-two-strings/1880-largest-merge-of-two-strings.py
+++ 1880-largest-merge-of-two-strings/1880-largest-merge-of-two-strings.py
@@ -10,7 +10,12 @@ class Solution:
                 res += word2[j]
                 j += 1
             else:
-                if word1[i:] > word2[j:]:
+                temp_i, temp_j = i, j
+                while temp_i < len(word1) and temp_j < len(word2) and word1[temp_i] == word2[temp_j]:
+                    temp_i += 1
+                    temp_j += 1
+                
+                if temp_j == len(word2) or (temp_i < len(word1) and word1[temp_i] > word2[temp_j]):
                     res += word1[i]
                     i += 1
                 else:
