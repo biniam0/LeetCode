4a64985d9dad068a152736b2bbb14ed4d31132f2
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-11-28 23:23:01 -0900

    Time: 43 ms (13.86%), Space: 28.5 MB (19.8%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,13 +1,16 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         count = Counter()
-        res = set()
+
         l = 0
         for r in range(9,len(s)):
             substring = s[l:r+1]
             count[substring] += 1
-
-            if substring not in res and count[substring] > 1:
-                res.add(substring)
             l += 1
-        return list(res)
+
+        res = []
+        for sub in count:
+            if count[sub] > 1:
+                res.append(sub)
+
+        return res
