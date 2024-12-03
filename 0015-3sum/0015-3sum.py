f314ff3e486a38c8c0f6822f6ae3fcbcd8f739d8
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-12-03 02:30:18 -0900

    Time: 579 ms (60.22%), Space: 20.3 MB (37.86%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,34 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = set()
-        n, p, z = [], [], []
-        for num in nums:
-            if num > 0:
-                p.append(num)
-            elif num < 0: 
-                n.append(num)
-            else:
-                z.append(num)
+        nums.sort()
+        n = len(nums)
+        res = []
 
-        N, P = set(n), set(p)
-        if z:
-            for num in P:
-                if -1*num in N:
-                    res.add((-1*num, 0, num))
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-        if len(z) >= 3:
-            res.add((0,0,0))
-
-        for i in range(len(n)):
-            for j in range(i+1,len(n)):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
-
-        for i in range(len(p)):
-            for j in range(i+1,len(p)):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i],p[j],target])))
+            l, r = i+1, n-1
+            while l < r:
+                sum_ = nums[i] + nums[l] + nums[r]
+                if sum_ < 0:
+                    l += 1
+                elif sum_ > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                   
 
         return res
