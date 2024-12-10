f63ab7fa62fb8e1e5c4fe8e6c6e0d92818067fed
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-12-10 01:06:29 -0900

    Time: 7 ms (90.54%), Space: 17.5 MB (5.52%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,14 +1,33 @@
 class Solution:
     def maximumLength(self, s: str) -> int:
-        s_freq = defaultdict(int)
-        ans = -1
+        top_3_freq = [[-1, -1, -1] for _ in range(26)]
 
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                s_freq[s[i:j+1]] += 1
+        wind_size = 0
+        last_seen = ""
 
-        for s, f in s_freq.items():
-            if len(set(s)) == 1 and f >= 3:
-                ans = max(ans, len(s))
+        for char in s:
+            idx = ord(char) - ord("a")
+            wind_size = wind_size + 1 if char == last_seen else 1
+            last_seen = char
+            min_idx = top_3_freq[idx].index(min(top_3_freq[idx]))
 
-        return ans
+            if wind_size > top_3_freq[idx][min_idx]:
+                top_3_freq[idx][min_idx] = wind_size
+
+        max_len = -1
+        for freq in top_3_freq:
+            max_len = max(max_len, min(freq))
+
+        return max_len
+        # s_freq = defaultdict(int)
+        # ans = -1
+
+        # for i in range(len(s)):
+        #     for j in range(i, len(s)):
+        #         s_freq[s[i:j+1]] += 1
+
+        # for s, f in s_freq.items():
+        #     if len(set(s)) == 1 and f >= 3:
+        #         ans = max(ans, len(s))
+
+        # return ans
