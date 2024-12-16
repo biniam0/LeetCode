ecb85422d31656d81394553aa8deaae18692d08b
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-12-16 01:27:54 -0900

    Time: 30 ms (63.73%), Space: 21.1 MB (5.73%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,23 +1,44 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        # Prefix + Hashmap
         n = len(nums)
+        prefix = {0: 1}
+        cur_sum = 0
+        ans = 0
 
-        def helper(x):
-            """
-                count num of subarrays where curSum <= x
-            """
-            if x < 0:
-                return 0
+        for r in range(n):
+            cur_sum += nums[r]
 
-            res = 0
-            l = cur = 0
-            for r in range(n):
-                cur += nums[r]
+            diff = cur_sum - goal
+            if diff in prefix:
+                ans += prefix[diff]
+            
+            if cur_sum in prefix:
+                prefix[cur_sum] += 1
+            else:
+                prefix[cur_sum] = 1
 
-                while cur > x:
-                    cur -= nums[l]
-                    l += 1
-                res += (r-l+1)
-            return res
+        return ans
 
-        return helper(goal) - helper(goal-1)
+        # n = len(nums)
+        # def helper(x):
+        #     """
+        #         count num of subarrays where curSum <= x
+        #     """
+        #     if x < 0:
+        #         return 0
+
+        #     res = 0
+        #     l = cur = 0
+        #     for r in range(n):
+        #         cur += nums[r]
+
+        #         while cur > x:
+        #             cur -= nums[l]
+        #             l += 1
+        #         res += (r-l+1)
+        #     return res
+
+        # return helper(goal) - helper(goal-1)
+
+        
