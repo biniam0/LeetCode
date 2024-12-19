ea2c14695819a8dc39c9877d135de045f2f82733
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-12-18 22:51:16 -0900

    Time: 0 ms (100%), Space: 18 MB (10.8%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,13 +1,26 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        n = len(prices)
-        res = []
+        # Monotonic Stack O(n)
+        res = prices.copy()
+        stack = []
 
-        for i in range(n):
-            for j in range(i+1, n):
-                if prices[i] >= prices[j]:
-                    res.append(prices[i]-prices[j])
-                    break
-            else:
-                res.append(prices[i])
+        for i in range(len(prices)):
+            while stack and prices[stack[-1]] >= prices[i]:
+                j = stack.pop()
+                res[j] -= prices[i]
+            stack.append(i)
+            res[i] = prices[stack[-1]]
+
         return res
+        # Brute force
+        # n = len(prices)
+        # res = []
+
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if prices[i] >= prices[j]:
+        #             res.append(prices[i]-prices[j])
+        #             break
+        #     else:
+        #         res.append(prices[i])
+        # return res
