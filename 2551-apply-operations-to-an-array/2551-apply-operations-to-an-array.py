172e186c5ddb001afce0e85ed7fb938642108097
biniam0 <153349728+biniam0@users.noreply.github.com>
2025-02-06 11:51:11 +0300

    Time: 167 ms (6.22%), Space: 17.9 MB (39.75%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -1,19 +1,27 @@
-class Solution(object):
-    def applyOperations(self, nums):
-        l = 0
-        for r in range(1, len(nums)):
-            if nums[l] == nums[r]:
-                nums[l] = nums[l] * 2
-                nums[r] = 0
-            l += 1
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        def helper(nums):  
+            l = 0
+            while l < n:
+                if nums[l] == 0:
+                    r = l
+                    while r < n and nums[r] == 0:
+                        r += 1
 
-        result = []
-        constant = 0
-        for num in nums:
-            if num != 0:
-                result.append(num)
-            else:
-                constant += 1
+                    if r < n:
+                        nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            return nums
 
-        return result + [0]*constant
-            
+        n = len(nums)
+        i = 0
+        while i <= n-2:
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+
+            i += 1
+
+        return helper(nums)
+
+    
