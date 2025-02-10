ac347d7891c1204de014640575d7662e36c5ab54
biniam0 <153349728+biniam0@users.noreply.github.com>
2025-02-10 22:57:42 +0300

    Time: 105 ms (86.62%), Space: 24.6 MB (60.3%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        heapq.heapify(nums)
+        ans = []
+        while nums:
+            ans.append(heapq.heappop(nums))
+        return ans
+            
