351f8686eed92ce64e593ae7decdc91d48f47b14
biniam0 <153349728+biniam0@users.noreply.github.com>
2025-02-10 23:04:48 +0300

    Time: 3 ms (13.29%), Space: 17.8 MB (49.75%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,27 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero = 0
-        one = 0
-        two = 0
-
-        for num in nums:
-            if num == 0:
-                zero += 1
-            elif num == 1:
-                one += 1
-            else:
-                two += 1
-
-        for i in range(len(nums)):
-            if zero > 0:
-                nums[i] = 0
-                zero -= 1
-            elif one > 0:
-                nums[i] = 1
-                one -= 1
-            else:
-                nums[i] = 2
-                two -= 1
-
+        for l in range(len(nums)):
+            for r in range(l+1, len(nums)):
+                if nums[l] > nums[r]:
+                    nums[l], nums[r] = nums[r], nums[l]
                 
