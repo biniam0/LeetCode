82d18d16c51eb38c51d947add4538dfaf546da5e
biniam0 <153349728+biniam0@users.noreply.github.com>
2025-02-12 16:27:04 +0300

    Time: 7 ms (14.29%), Space: 17.8 MB (61.52%) - LeetHub

--- 1009-pancake-sorting/1009-pancake-sorting.py
+++ 1009-pancake-sorting/1009-pancake-sorting.py
@@ -1,25 +1,21 @@
-class Solution(object):
-    def pancakeSort(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: List[int]
-        """
-        end = len(arr)
-        res = []
-
-        while end > 1:
-            max_idx = arr.index(end)
-
-            if max_idx == end - 1:
-                end -= 1
-                continue
-
-            if max_idx != 0:
-                arr[:max_idx + 1] = reversed(arr[:max_idx + 1])
-                res.append(max_idx+1)
-
-            arr[:end] = reversed(arr[:end])
-            res.append(end)
-            end -= 1
-
-        return res
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        rnd, n, ans = 0, len(arr), []
+        while rnd < n:
+            max_idx = arr.index(max(arr[:n-rnd]))
+            if max_idx != n-rnd-1:
+                if max_idx != 0:
+                    ans.append(max_idx+1)
+                    l, r = 0, max_idx
+                    while l < r:
+                        arr[l], arr[r] = arr[r], arr[l]
+                        l += 1
+                        r -= 1
+                ans.append(n-rnd)
+                i, j = 0, n-rnd-1
+                while i < j and j < n:
+                    arr[i], arr[j] = arr[j], arr[i]
+                    i += 1
+                    j -= 1
+            rnd += 1
+        return ans
