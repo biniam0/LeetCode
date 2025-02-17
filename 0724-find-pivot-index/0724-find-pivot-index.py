a34a92b796fc5b8d128c176e2a52a2be538812a7
biniam0 <153349728+biniam0@users.noreply.github.com>
2025-02-17 16:16:22 +0300

    Time: 3 ms (87.42%), Space: 18.6 MB (78.05%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,29 +1,10 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        total_sum = sum(nums) 
-        left_sum = 0
-
-        for i, num in enumerate(nums):
-            if left_sum == (total_sum - left_sum - num):
+        tot = sum(nums)
+        left = 0
+        for i in range(len(nums)):
+            tot -= nums[i]
+            if tot == left:
                 return i
-            left_sum += num
-
+            left += nums[i]
         return -1
-
-
-
-    #     n = len(nums)
-    #     prefix = [0]*n
-    #     postfix = [0]*n
-
-    #     for i in range(1, n):
-    #         prefix[i] = nums[i-1] + prefix[i-1]
-
-    #     for i in range(n-2, -1, -1):
-    #         postfix[i] = nums[i+1] + postfix[i+1]
-
-    #     for i in range(n):
-    #         if prefix[i] == postfix[i]:
-    #             return i
-
-    #     return -1
